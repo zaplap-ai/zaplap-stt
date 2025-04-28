7840d4696c22453ae8a89a0830c243bd739bacbb
sivaprakash-anand06 <70039629+sivaprakash-anand06@users.noreply.github.com>
2025-04-28 23:38:51 +0530

    updated model

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-model = whisper.load_model("base")
+model = whisper.load_model("tiny")
 def whisper_stt(audio_file):
     """
     Pass audio file to get text output
     """
-    logging.info("getting trascription")
+    logging.info("getting transcribe...")
     result = model.transcribe(audio_file, task="transcribe", language="en")
     return result['text']
 
